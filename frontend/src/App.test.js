80f61ebde4a9c361697509b5a52e5c1323655e9a
Urmila-devda <125447725+Urmila-devda@users.noreply.github.com>
2026-05-21 20:24:38 +0530

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders brand name in header", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/NailArt Studio/i);
-  expect(linkElement).toBeInTheDocument();
-});
